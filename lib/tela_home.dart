13a20def63ba98387aa7ab0c945ef2e86121fb78
GabrielBrito770 <gabrielsbrito07@gmail.com>
2025-09-01 16:01:42 -0300

    Criação do método de autenticação

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TelaHome extends StatelessWidget{
-  TelaHome([super.key]);
+  TelaHome({super.key});
 
   @override
   Widget build(BuildContext context) {
